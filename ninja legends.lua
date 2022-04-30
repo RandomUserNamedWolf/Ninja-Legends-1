52656d525b89864c20bd9f33ca615cf8eacce9ba
RandomUserNamedWolf <78046643+RandomUserNamedWolf@users.noreply.github.com>
2022-04-30 23:09:59 +0100

    Complete UI change

--- ninja legends.lua
+++ ninja legends.lua
@@ -1,34 +1,41 @@
-if game.PlaceId == 3956818381 then
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("ToxicHub: Ninja Legends 1", "Ocean")
+local Finity = loadstring(game:HttpGet("https://pastebin.com/raw/BbxzJKTw"))()
+local FinityWindow = Finity.new(true) -- btw true is dark mode! dont forget dumbass
+FinityWindow.ChangeToggleKey(Enum.KeyCode.Semicolon)
 
+local Updatelog1 = FinityWindow:Category("Home")
+local Player1 = FinityWindow:Category("Player")
+local Autofarm1 = FinityWindow:Category("Autofarm")
+local AutoBuy1 = FinityWindow:Category("Autobuy")
+local Teleport1 = FinityWindow:Category("Teleports")
+-- --Sections
+local Player = Player1:Sector("Changes")
+local Autofarm = Autofarm1:Sector("Autofarm")
 
-    -- Player tab dont do anything here! :D
-    local Player = Window:NewTab("Player")
-    local PlayerSection = Player:NewSection("Player")
-    PlayerSection:NewSlider("JumpPower", "Changes the jumppower", 250, 16, function(v)
-        game.Players.LocalPlayer.Character.HumanoidRootPart.JumpPower = v
-    end)
-    PlayerSection:NewSlider("Walkspeed", "Changes the Walkspeed", 250, 50, function(v)
-        game.Players.LocalPlayer.Character.HumanoidRootPart.Walkspeed = v
-    end)
+--Autobuy
+local Autobuy = AutoBuy1:Sector("Auto buy")
 
+-- Teleports
+local Teleport = Teleport1:Sector("Unlock all islands")
+local Teleports = Teleport1:Sector("Teleport Island")
+local Teleport3 = Teleport1:Sector("Light karma training areas")
+local Teleport4 = Teleport1:Sector("Bad karma training areas")
+--Updates/Home
+local Credits = Updatelog1:Sector("Credits")
+local Updatelog = Updatelog1:Sector("Update log")
 
-    -- Autofarm area
-    local Autofarm = Window:NewTab("Autofarm")
-    local AutofarmSection = Autofarm:NewSection("Autofarm")
-
-    AutofarmSection:NewToggle("Auto Swing", "Makes you auto swing", function(v)
+-- Autofarm shit
+Autofarm:Cheat("Checkbox", "Auto swing", function(v)
         getgenv().autoswing = v
         while true do
-        if not getgenv().autoswing then return end 
+        if not getgenv().autoswing then return end
         local A_1 = "swingKatana"
         local Event = game:GetService("Players").LocalPlayer.ninjaEvent
         Event:FireServer(A_1)
         wait(0.2)
         end
     end)
-    AutofarmSection:NewToggle("Auto Shuriken", "Makes you throw shurikens", function(v)
+
+Autofarm:Cheat("Checkbox", "Auto Shuriken", function(v)
         getgenv().autoshuriken = v
         while true do
         if not getgenv().autoshuriken then return end
@@ -38,11 +45,12 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
         wait(0.5)
         end
     end)
-    AutofarmSection:NewToggle("Auto Sell", "Auto sell for money", function(v)
-        getgenv().autosell = v
+
+Autofarm:Cheat("Checkbox", "Auto Sell", function(v)
+    getgenv().autosell = v
         print(v)
         while true do
-            if getgenv().autoswing == false then
+            if getgenv().autosell == false then
                 print(getgenv().autosell)
                 return
             end
@@ -51,10 +59,12 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
             game:GetService("Workspace").sellAreaCircles["sellAreaCircle16"].circleInner.CFrame = CFrame.new(0,0,0)
         end
     end)
-    AutofarmSection:NewToggle("Auto buy swords", "Buys all swords", function(v)
-        getgenv().buyswords = v
+
+    -- auto buy shit
+    Autobuy:Cheat("Checkbox", "Buy Swords", function(v)
+        getgenv().buysword = v
         while true do
-            if not getgenv().buyswords then return end
+            if not getgenv().buysword then return end
             local A_1 = "buyAllSwords"
             local A_2 = "Blazing Vortex Island"
             local Event = game:GetService("Players").LocalPlayer.ninjaEvent
@@ -63,7 +73,7 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
         end
     end)
 
-    AutofarmSection:NewToggle("Auto buy belts", "Buys all belts", function(v)
+    Autobuy:Cheat("Checkbox", "Buy Belts", function(v)
         getgenv().buybelts = v
         while true do
             if not getgenv().buybelts then return end
@@ -74,42 +84,49 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
             wait(0.5)
         end
     end)
-    AutofarmSection:NewToggle("Auto buy skills", "Auto buys all skills", function(v)
-        getgenv().buyskills = v
-        while true do
-            if not getgenv().buyskills then return end
-            local A_1 = "buyAllSkills"
-            local A_2 = "Blazing Vortex Island"
-            local Event = game:GetService("Players").LocalPlayer.ninjaEvent
-            Event:FireServer(A_1, A_2)
-            wait(0.5)
-        end
-    end)
-    AutofarmSection:NewToggle("Auto buy shurikens", "Auto buys all shuikens", function(v)
-        getgenv().buyshurikens = v
-        while true do
-            if not getgenv().buyshurikens then return end
-            local A_1 = "buyAllShurikens"
-            local A_2 = "Blazing Vortex Island"
-            local Event = game:GetService("Players").LocalPlayer.ninjaEvent
-            Event:FireServer(A_1, A_2)
-            wait(0.5)
-        end
-    end)
+
+        Autobuy:Cheat("Checkbox","Buy Ranks", function(v)
+            getgenv().buyrank = v
+                while true do
+                    if not getgenv().buyrank then return end
+                    local A_1 = "buyRank"
+                    local A_2 = game:GetService("ReplicatedStorage").Ranks.Ground:GetChildren()
+                    for i = 1,#A_2 do
+                    game:GetService("Players").LocalPlayer.ninjaEvent:FireServer(A_1, A_2[i].Name)
+                    end
+                    wait()
+                end
+            end)
+            
+            Autobuy:Cheat("Checkbox", "Buy Skills", function(v)
+                getgenv().buyskills = v
+                while true do
+                    if not getgenv().buyskills then return end
+                    local A_1 = "buyAllSkills"
+                    local A_2 = "Blazing Vortex Island"
+                    local Event = game:GetService("Players").LocalPlayer.ninjaEvent
+                    Event:FireServer(A_1, A_2)
+                    wait(0.5)
+                end
+            end)
+
+    -- Player shit
+    Player:Cheat("Walkspeed","Walkspeed", function(b)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.Walkspeed = b
+    end, {min = 16, max = 120, suffix = "t"})
 
 
+    --Teleport shit
 
-    local Teleports = Window:NewTab("Teleports")
-    local TeleportSection = Teleports:NewSection("Teleports")
-    TeleportSection:NewButton("Unlock all island", "Unlock all islands", function()
+    Teleport:Cheat("Button", "Unlock all islands", function()
         for _,v in pairs(game:GetService("Workspace").islandUnlockParts:GetChildren()) do
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
             wait(0.2)
         end
-        game.PLayers.LocalPlayer.Character.HumanoidRootPart.CFrame = oldcframe
+        game.PLayers.LocalPlayer.Character.HumanoidRootPart.CFrame = oldCFrame
     end)
 
-    TeleportSection:NewDropdown("Teleports", "DropdownInf", {"Enchanted island", "Astral island", "Mystical island", "Space island", "Tundra island", "Eternal island", "Sandstorm island", "Thunderstorm island", "Ancient Inferno island", "Midnight Shadow Island", "Mythical Souls island", "Winter Wonderland island", "Golden Master island", "Dragon Legend island", "Cybernetic Legends island", "Skystorm Ultraus island", "Chaos Legends island", "Soul Fusion island", "Dark Elements island", "Inner Peace Island", "Blazing Vorterx island"}, function(SelectLocation)
+    Teleports:Cheat("Dropdown", "Teleport to Island", function(SelectLocation)
         if SelectLocation == "Enchanted island" then
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(51.242, 849.832, -151.814)
         elseif SelectLocation == "Astral island" then
@@ -153,5 +170,72 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
         elseif SelectLocation == "Blazing Vorterx island" then
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(144.021, 91354.5, 88.9619)
         end
-    end)
-end
+    end, {
+            options ={
+                "Enchanted island",
+                "Astral island",
+                "Mystical island",
+                "Space island",
+                "Tundra island",
+                "Eternal island",
+                "Sandstorm island",
+                "Thunderstorm island",
+                "Ancient Inferno island",
+                "Midnight Shadow Island",
+                "Mythical Souls island",
+                "Winter Wonderland island",
+                "Golden Master island",
+                "Dragon Legend island",
+                "Cybernetic Legends island",
+                "Skystorm Ultraus island",
+                "Chaos Legends island",
+                "Soul Fusion island",
+                "Dark Elements island",
+                "Inner Peace island",
+                "Blazing Vorterx island",
+            }
+        })
+
+                Teleport3:Cheat("Dropdown", "Light karma training areas", function(TeleportLight)
+                    if TeleportLight == "Mystical Waters" then
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(354.478, 8874.26, 116.569)
+                    elseif TeleportLight == "Sword Of Legends" then
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1848.48, 82.2062, -137.431)
+                    elseif TeleportLight == "Elemental Tornado" then
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(330.128, 30432.8, -16.1308)
+                    elseif TeleportLight == "Zen Master's Blade" then
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5041.18, 82.2062, 1614.75)
+                    end
+                end, {
+                    options ={
+                        "Mystical Waters",
+                        "Sword Of Legends",
+                        "Elemental Tornado",
+                        "Zen Master's Blade",
+                    
+                    }
+                })
+
+                Teleport4:Cheat("Dropdown", "Bad karma training areas", function(TeleportBad)
+                    if TeleportBad == "Tornado" then
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(330.128, 16921.8, -16.1308)
+                    elseif TeleportBad == "Lava pit" then
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-119.522, 13002.3, 274.569)
+                    elseif TeleportBad == "Swords Of Ancients" then
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(627.359, 82.2062, 2429.49)
+                    elseif TeleportBad == "Fallen Infinity Blade" then
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1875.97, 82.2062, -6806.41)
+                    end
+                end, {
+                    options ={
+                        "Tornado",
+                        "Lava pit",
+                        "Swords Of Ancients",
+                        "Fallen Infinity Blade",
+                    }
+                })
+
+    -- Home
+
+    Updatelog:Cheat("Label", "Release :)")
+    Credits:Cheat("Label", "RealWolfnx")
